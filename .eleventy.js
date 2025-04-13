1b9d062c0f55b411a218549d127c96dbd3bcb2ef
Capitaine Prompt <stephan.fraysse@gmail.com>
2025-04-13 17:40:20 +0200

    Initialisation du projet Histoires Interactives avec Eleventy

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig) {
+
+    // Dit à Eleventy où trouver les fichiers (templates, data, includes)
+    return {
+      dir: {
+        input: "src",         // Notre dossier source
+        output: "_site",      // Le dossier où le site sera généré (Eleventy le crée pour nous)
+        includes: "_includes", // Le dossier pour les bouts de code réutilisables
+        data: "_data"         // Le dossier pour les données (comme notre JSON)
+      }
+    };
+  };
---
+++ src/index.md
@@ -0,0 +1,8 @@
+---
+layout: null # On verra les layouts plus tard, pour l'instant pas de modèle
+---
+# Bienvenue sur Histoires Interactives
+
+Voici notre première histoire :
+
+*   [L'île aux Mystères](/histoires/ile-mystere/start/)
